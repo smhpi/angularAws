fb31381a72a66f80131dc80ea69481c922c457f6
Hosen Poursaeid <poursaeid@outlook.com>
2019-01-07 21:49:57 -0500

    navbar-bootstrap

---
+++ src/app/app-nav.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-nav",
+  templateUrl: "./app-nav.component.html",
+  styleUrls: ["./app-nav.component.css"]
+})
+export class NavbarComponent {}
